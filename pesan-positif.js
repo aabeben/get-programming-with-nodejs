332c54a4587900124591b38a856ecbc4167083c5
Beny Hasanudin <bhasanudin@gmail.com>
2020-06-27 13:57:43 +0700

    pesan-positif.js adalah hasil dari perintah .save pada NodeJs REPL

---
+++ pesan-positif.js
@@ -0,0 +1,8 @@
+"use strict";
+let pesanPesan = [
+	"Sebuah perubahan pada lingkungan dapat menjadi sebuah hal yang bagus!",
+		"Kamu akan berhasil!",
+			"Cukup larikan kodenya!"
+			];
+			
+pesanPesan.forEach(pesan=> console.log(pesan));
