cb4d14cf90414b6fffe181b84c8809127fad150f
Beny Hasanudin <bhasanudin@gmail.com>
2020-06-28 10:17:15 +0700

    Rute sederhana di dalam server web di berkas main.js

--- rute-rute-sederhana/main.js
+++ rute-rute-sederhana/main.js
@@ -1,26 +1,22 @@
 "use strict";
-const getJSONString = obj => JSON.stringify(obj, null, 2);
+const petaRuteTanggapan = {
+	"/info":"<h1>Halaman Info</h1>",
+	"/kontak":"<h1>Halaman Kontak</h1>",
+	"/tentang":"<h1>Halaman Tentang Kami</h1>",
+	"/salam":"<h1>Halaman Salam</h1>",
+	"/galat":"<h1>Maaf halaman yang anda cari tidak ada di sini.</h1>",
+};
 const port = 3000,
 	http = require("http"),
 	statusHttp = require("http-status-codes"),
-	app = http.createServer();
-
-app.on("request", (req, res)=>{
-	var badan = [];
-	req.on("data",(dataBadan)=> badan.push(dataBadan));
-	req.on("end",()=>{
-		badan = Buffer.concat(badan).toString();
-		console.log(`Isi-isi dari badan permintaan: ${badan}`);
+	app = http.createServer((req, res)=>{
+		res.writeHead(200, {
+			"Content-Type":"text/html",
+		});
+		if(petaRuteTanggapan[req.url]){
+			res.end(petaRuteTanggapan[req.url]);
+		} else {
+			res.end("<h1>Selamat Datang!</h1>");
+		}
 	});
-	res.writeHead(statusHttp.OK,{
-	  "Content-Type":"text/html",
-	});
-	console.log(`Method: ${getJSONString(req.method)}`);
-	console.log(`URL: ${getJSONString(req.url)}`);
-	console.log(`Headers: ${getJSONString(req.headers)}`);
-	let pesanTanggapan = "<h1>Ini akan tampil pada layar.</h1>";
-	res.end(pesanTanggapan);
-});// app.on method ends.
-
-app.listen(port);
-console.log(`Server telah dimulai dan sedang mendengarkan pada port ${port}`);
+app.listen(port, ()=> console.log(`Server telah dimulai dan mendengarkan pada port: ${port}`));
